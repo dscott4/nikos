4f76160bc409f9e6056099bf0efc2cb869524bf5
dscott4 <davidkhscott@gmail.com>
2017-11-04 15:07:38 +0000

    new data tings
    david

--- preprocessing/time_series_addition.m
+++ preprocessing/time_series_addition.m
@@ -3,13 +3,14 @@ function [ dataset] = time_series_addition( dataset,day_points,start_year )
 %year, the second is a day of week column starting from a Sunday, 7.
 [m_old,n_old] = size(dataset);
 years = ceil(m_old/(day_points*365));
-[dataset] = [ones(m_old,2) dataset];
-day_sequence = [7*ones(1,day_points),1*ones(1,day_points),2*ones(1,day_points),3*ones(1,day_points),4*ones(1,day_points),5*ones(1,day_points),6*ones(1,day_points)];
+[dataset] = [ones(m_old,1) dataset];
+day_sequence = [1*ones(1,day_points),2*ones(1,day_points),3*ones(1,day_points),4*ones(1,day_points),5*ones(1,day_points),6*ones(1,day_points),7*ones(1,day_points),];
 day_sequence = day_sequence';
 day_sequence = repmat(day_sequence,[(round(m_old/(day_points*7))+1) 1]);
 day_sequence = day_sequence(1:m_old,:);
-dataset(:,2) = day_sequence;
+dataset(:,1) = day_sequence;
 year_sequence = [];
+%{
 for i = 1:years
     if mod(start_year,4) == 0
         year_array = ceil([1:(day_points*365)]./day_points);
@@ -20,5 +21,6 @@ for i = 1:years
 end
 year_sequence = year_sequence';
 dataset(:,1) = year_sequence(1:m_old,:);
+%}
 end
 
--- real_tings/preprocessing2.m
+++ real_tings/preprocessing2.m
@@ -1,8 +1,8 @@
 data_locations = 'Round 1 Data FC.xlsx';
 [dataset] = load_npower_data(data_locations);
-dataset(:,8) = [];
+dataset = dataset(1:731,:);
 interp_pts = 0; %Set to 0 if no interpolation
 %[dataset] = interpolant_creator(dataset,interp_pts,'cubic');
 [dataset] = time_series_addition(dataset,1*(interp_pts+1),2012);
-[dataset,mu,sigma] = data_normalisation(dataset);
+%[dataset,mu,sigma] = data_normalisation(dataset);
 %[ R,cov ] = correlation_covariance(dataset,false);
