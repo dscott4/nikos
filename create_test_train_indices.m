192f539163495bbda206418a848710990a00acff
dscott4 <davidkhscott@gmail.com>
2017-11-04 16:19:18 +0000

    test train

--- create_test_train_indices.m
+++ create_test_train_indices.m
@@ -2,7 +2,7 @@ function [ testInd,trainInd ] = create_test_train_indices(m,trainRatio)
 evens = [2:2:m];
 all = [1:1:m];
 randNums = randperm(length(evens));
-trainInd = evens(randNums(1:round(length(evens)*trainRatio)));
-testInd=setdiff(all,trainInd);
+testInd = evens(randNums(1:round((m)*(1-trainRatio))));
+trainInd=setdiff(all,testInd);
 end
 
