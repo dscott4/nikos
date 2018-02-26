b4a9ae2ef4fb01dac467a3b126868870150b08c8
dscott4 <davidkhscott@gmail.com>
2018-02-26 16:34:39 +0000

    lol
    gp tings

--- real_tings/preprocessing3.m
+++ real_tings/preprocessing3.m
@@ -1,4 +1,4 @@
-function [X_test,y_test,X_train,y_train,X_val,y_val,dataset,mu,sigma] = preprocessing3(testRatio,trainRatio)
+function [X_test,y_test,X_train,y_train,X_val,y_val,dataset,mu,sigma,trainInd,testInd,valInd] = preprocessing3(testRatio,trainRatio)
 data_locations = 'Round 3 Data FC.xlsx';
 pts = 914;
 
@@ -9,12 +9,12 @@ end
 trainRatio = trainRatio/testRatio;
 
 [dataset] = load_npower_data(data_locations);
-testPts = [round(testRatio*pts):1:pts];
-[valPts,trainPts] = create_test_train_indices(round(testRatio*pts)-1,trainRatio);
+testInd = [round(testRatio*pts):1:pts];
+[valInd,trainInd] = create_test_train_indices(round(testRatio*pts)-1,trainRatio);
 
 dataset = dataset(1:pts,:);
 [dataset,mu,sigma] = data_normalisation(dataset);
-[X_test,y_test] = data_separation(dataset(testPts,:));
-[X_train,y_train] = data_separation(dataset(trainPts,:));
-[X_val,y_val] = data_separation(dataset(valPts,:));
+[X_test,y_test] = data_separation(dataset(testInd,:));
+[X_train,y_train] = data_separation(dataset(trainInd,:));
+[X_val,y_val] = data_separation(dataset(valInd,:));
 end
